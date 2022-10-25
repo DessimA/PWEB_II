a506dcc794e46c45db62fb93b670fce74a847efb
José Anderson da Silva Costa <60760405+DessimA@users.noreply.github.com>
2022-10-25 13:40:10 -0300

    add twitter_post

---
+++ twitter_post/js/app.js
@@ -0,0 +1,46 @@
+// criar post
+const inputTwitter = document.querySelector("#input-twitter");
+
+const btnAdd = document.querySelector("#btn-add");
+
+const twitterBox = document.querySelector(".tweetBox");
+
+const postTwitter = document.querySelector("#post-twitter");
+
+btnAdd.onclick = function (e) {
+  e.preventDefault();
+
+  const newPost = `
+  <div class="post">
+        <div class="post__avatar">
+          <img
+            src="https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png"
+            alt=""
+          />
+        </div>
+
+        <div class="post__body">
+          <div class="post__header">
+            <div class="post__headerText">
+              <h3>
+                Somanath Goudar
+                <span class="post__headerSpecial"
+                  ><span class="material-icons post__badge"> verified </span>@somanathg</span>
+                </h3>
+            </div>
+            <div class="post__headerDescription">
+              <p>${inputTwitter.value}</p>
+            </div>
+          </div>
+          
+          <div class="post__footer">
+            <span class="material-icons"> repeat </span>
+            <span class="material-icons"> favorite_border </span>
+            <span class="material-icons"> publish </span>
+          </div>
+        </div>
+      </div>
+  `;
+  postTwitter.insertAdjacentHTML("afterbegin", newPost);
+  inputTwitter.value = "";
+};
